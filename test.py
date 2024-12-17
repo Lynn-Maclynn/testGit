be60e30c6610b7327897689eaec7fe318d0eb8c3
Lynn Maclynn <magnyanounecynthialynn@gmail.com>
2024-12-17 04:07:24 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+
+print("hello, my name is Cynthia")
+print("Nous allons faire des petits calculs")
+t = 5+3
+s = 2-9
+u = 20/3
+h=20//3
+msg = "C'est même quoi ?"
+print("h =", h)
+print("t =", t)
+print("s =", s)
+print("u =", u)
+print(msg)
+
+print("EXERCICES")
+a,b,c = 3,7,5  # La meilleure commande de la journée
+z = a-b//c
+print("Le résutat est z = ", z)
+r,pi = 12,3.14
+ss = pi*r**2
+print("ss = ", ss)
+print(type(ss), type(pi), type(r))
